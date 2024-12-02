4520784e294c52df5367e4d5be75bfef545a06ee
juanjov04 <juan.villalba.ruiz@udla.edu.ec>
2024-12-02 08:47:56 -0500

    cambios de la parte 1

--- VillalbaJuanJose/MainPage.xaml.cs
+++ VillalbaJuanJose/MainPage.xaml.cs
@@ -1,25 +1,67 @@
-﻿namespace VillalbaJuanJose
+﻿using System.Data;
+using 
+
+namespace VillalbaJuanJose
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private string ultimaRecarga;
+        public string UltimaRecarga
+        {
+            get => ultimaRecarga;
+            set
+            {
+                ultimaRecarga = value;
+                OnPropertyChanged(nameof(UltimaRecarga));
+            }
+        }
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+            CargarUltimaRecarga();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnRecargarClicked(object sender, EventArgs e)
         {
-            count++;
+            string nombre = vjj_entrynombre.Text;
+            string numero = vjj_entrytelefono.Text;
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(numero))
+            {
+                DisplayAlert("Error", "Por favor, ingresa todos los datos", "OK");
+                return;
+            }
+            string nombreArchivo = $"{nombre.Replace("", "")}.txt";
+            string rutaArchivo = Path.Combine(FileSystem.AppDataDirectory, nombreArchivo);
+            string contenido = $"Nombre: {nombre}\nNumero:{numero}\nFecha:{DateTime.Now}";
+            File.WriteAllText(rutaArchivo, contenido);
+            DisplayAlert("Exito", "La recarga fue exitosa", "OK");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            UltimaRecarga=contenido;
+            vjj_entrynombre.Text = string.Empty;
+            vjj_entrytelefono.Text = string.Empty;
         }
-    }
+        private void CargarUltimaRecarga()
+        {
+            try
+            {
+                string[] archivos = Directory.GetFiles(FileSystem.AppDataDirectory, "*.txt");
+                if (archivos.Length > 0)
+                {
+                    string ultimoArchivo = archivos[^1];
+                    UltimaRecarga = File.ReadAllText(ultimoArchivo);
+                }
+                else
+                {
+                    UltimaRecarga = "No hay recargas registradas";
+                }
+            }
+            catch (Exception ex) 
+            {
+                UltimaRecarga = $"Error al cargar recarga: {ex.Message}";
+            }
+        }
 
+
+    }
 }
