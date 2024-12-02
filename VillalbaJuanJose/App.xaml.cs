253e55c4e8c943a27c2c08203ec4832dfc614381
juanjov04 <juan.villalba.ruiz@udla.edu.ec>
2024-12-02 07:18:36 -0500

    Agregar archivos de proyecto.

---
+++ VillalbaJuanJose/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VillalbaJuanJose
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VillalbaJuanJose/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VillalbaJuanJose
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
